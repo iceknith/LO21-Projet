59fc0c63b77324139591f8f62e0856ee94a4b1f3
iceknith <iceknith@users.noreply.github.com>
2025-12-13 11:53:31 +0000

    deploy: 579ec76ab5e341de9c1ae96e234d9ba9936b0b02

--- search/all_c.js
+++ search/all_c.js
@@ -5,5 +5,6 @@ var searchData=
   ['nombrejoueursmax_2',['nombreJoueursMax',['../dd/d95/namespaceconstJeu.html#a40761edea300ca8870a0c5c103aae3ef',1,'constJeu']]],
   ['nombretours_3',['nombreTours',['../d1/de1/classJeu.html#a090a1fc4bfe29c48d97903990196c9a7',1,'Jeu']]],
   ['nombretuiles_4',['nombreTuiles',['../df/d51/classChantier.html#abefa1198f791023d5ebe61122c17af27',1,'Chantier::nombreTuiles()'],['../d6/df2/classDeck.html#ac9523619303bfd9d62abf010ca5529b4',1,'Deck::nombreTuiles()']]],
-  ['normale_5',['NORMALE',['../d7/d09/Utils_8hpp.html#ad3df9804641f6ea0105cd2a89f495555a1758932d18841976caf0cf463ef1bee1',1,'Utils.hpp']]]
+  ['nomjoueur_5',['nomJoueur',['../dd/d07/classJoueur.html#ac9de73063d1f35b745e0e8e78f91f0e7',1,'Joueur']]],
+  ['normale_6',['NORMALE',['../d7/d09/Utils_8hpp.html#ad3df9804641f6ea0105cd2a89f495555a1758932d18841976caf0cf463ef1bee1',1,'Utils.hpp']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -4,5 +4,6 @@ var searchData=
   ['nombrejoueurs_1',['nombreJoueurs',['../d1/de1/classJeu.html#ad57abd13a38c6a2f984ace537fcfdc4e',1,'Jeu']]],
   ['nombrejoueursmax_2',['nombreJoueursMax',['../dd/d95/namespaceconstJeu.html#a40761edea300ca8870a0c5c103aae3ef',1,'constJeu']]],
   ['nombretours_3',['nombreTours',['../d1/de1/classJeu.html#a090a1fc4bfe29c48d97903990196c9a7',1,'Jeu']]],
-  ['nombretuiles_4',['nombreTuiles',['../df/d51/classChantier.html#abefa1198f791023d5ebe61122c17af27',1,'Chantier::nombreTuiles()'],['../d6/df2/classDeck.html#ac9523619303bfd9d62abf010ca5529b4',1,'Deck::nombreTuiles()']]]
+  ['nombretuiles_4',['nombreTuiles',['../df/d51/classChantier.html#abefa1198f791023d5ebe61122c17af27',1,'Chantier::nombreTuiles()'],['../d6/df2/classDeck.html#ac9523619303bfd9d62abf010ca5529b4',1,'Deck::nombreTuiles()']]],
+  ['nomjoueur_5',['nomJoueur',['../dd/d07/classJoueur.html#ac9de73063d1f35b745e0e8e78f91f0e7',1,'Joueur']]]
 ];
