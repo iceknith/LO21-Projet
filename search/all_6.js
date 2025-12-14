3eb3940085f50fbe3fbc8c1bf9b1bd5dec3d9a95
CMail4 <CMail4@users.noreply.github.com>
2025-12-14 20:40:09 +0000

    deploy: bd2f69bca0eabb1c99acb043eea47d7e821d44de

--- search/all_6.js
+++ search/all_6.js
@@ -33,7 +33,7 @@ var searchData=
   ['getecranselectionnombrejoueurs_30',['getEcranSelectionNombreJoueurs',['../d6/d1a/classMainWindow.html#a864dec0c584c6a7521b27eb15feeabfb',1,'MainWindow']]],
   ['getecranselectionsauvegarde_31',['getEcranSelectionSauvegarde',['../d6/d1a/classMainWindow.html#a1983e4caf5a348b57a3b0d566b47ccd8',1,'MainWindow']]],
   ['getecrantitre_32',['getEcranTitre',['../d6/d1a/classMainWindow.html#a4edf53deb3612fb59835c136079fb6d1',1,'MainWindow']]],
-  ['geteictoire_33',['getEictoire',['../d6/d1a/classMainWindow.html#a69493cb03192820d23b154aa5bbd8386',1,'MainWindow']]],
+  ['getecranvictoire_33',['getEcranVictoire',['../d6/d1a/classMainWindow.html#a1eda3c814ef519b05f788007e3266823',1,'MainWindow']]],
   ['gethexagone_34',['getHexagone',['../dc/d32/classHexagoneContainer.html#acdc01bcf915352cc0d779fbf89af6e5d',1,'HexagoneContainer']]],
   ['getinstance_35',['getInstance',['../d1/d3c/classAffichageConsole.html#aa81c3f205d74e2f88b40723f04668381',1,'AffichageConsole']]],
   ['getjeu_36',['getJeu',['../d1/de1/classJeu.html#a3183656c8617b3f8c9366c2141de164e',1,'Jeu::getJeu()'],['../da/d36/classJeuGUI.html#af64b6e2baebe501fb255bfa33c12e90a',1,'JeuGUI::getJeu()'],['../dc/dcd/classJeuConsole.html#a214e0b1658a9fea8876ada883284ca64',1,'JeuConsole::getJeu()']]],
