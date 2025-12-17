bf313169e3459e11be238938b999e9a644dfade4
Mineplex440 <Mineplex440@users.noreply.github.com>
2025-12-17 19:49:27 +0000

    deploy: 8e14b813fa929c1d44a410b13269d36d8fea397a

--- search/variables_8.js
+++ search/variables_8.js
@@ -1,12 +1,18 @@
 var searchData=
 [
-  ['labelnom_0',['labelNom',['../de/d5c/classAffichageGUI.html#acb1f880f91c942db22f842535fda2182',1,'AffichageGUI::labelNom()'],['../d2/dc8/classEcranJeu.html#a606396df8d1c7b71f2fd07574e8d10c7',1,'EcranJeu::labelNom()']]],
-  ['labelpierre_1',['labelPierre',['../de/d5c/classAffichageGUI.html#ac8f48a7d4ec4c59750da61e0d3179f90',1,'AffichageGUI::labelPierre()'],['../d2/dc8/classEcranJeu.html#a7b639379d87904c90fccc3ce20782876',1,'EcranJeu::labelPierre()']]],
-  ['labelreglescore_2',['labelRegleScore',['../d2/dc8/classEcranJeu.html#a7ea4b88187ff46a543c465d30e71c394',1,'EcranJeu']]],
-  ['labelschantier_3',['labelsChantier',['../d2/dc8/classEcranJeu.html#aa0c711cd1135541316ac640719a4c4c1',1,'EcranJeu']]],
-  ['labelscore_4',['labelScore',['../de/d5c/classAffichageGUI.html#aa183aef7c604f92b7e8d1d1b032eb330',1,'AffichageGUI::labelScore()'],['../d2/dc8/classEcranJeu.html#a2f2d8bffd93c1ac807623a31f632d4b2',1,'EcranJeu::labelScore()']]],
-  ['largeurcentre_5',['largeurCentre',['../d3/d75/namespaceconstAffichageConsoleHex.html#ad1c2459cb2060d8fb56216a8f78739ac',1,'constAffichageConsoleHex']]],
-  ['largeurhex_6',['largeurHex',['../d3/d75/namespaceconstAffichageConsoleHex.html#a8d058f2a67cfa42c273b74eaea82fe72',1,'constAffichageConsoleHex']]],
-  ['layoutchampssaisies_7',['layoutChampsSaisies',['../d9/ddf/classEcranSaisieNoms.html#a10c8c05c2aad8aee768c9726cbcc6f29',1,'EcranSaisieNoms']]],
-  ['localpos_8',['localPos',['../d4/dfc/classHexagone.html#a3c72d948f768a1a9804c41fe27d24009',1,'Hexagone']]]
+  ['labelj1_0',['labelJ1',['../d2/dc8/classEcranJeu.html#ac42b736aad052beee43cc80f39bb2459',1,'EcranJeu']]],
+  ['labelj2_1',['labelJ2',['../d2/dc8/classEcranJeu.html#a04d9d815614ffa6aec7253a6747237ad',1,'EcranJeu']]],
+  ['labelj3_2',['labelJ3',['../d2/dc8/classEcranJeu.html#a8e94ee88416bf23df9acdb0f61ce4cb3',1,'EcranJeu']]],
+  ['labelj4_3',['labelJ4',['../d2/dc8/classEcranJeu.html#aad414fe3c9b349ee74349eab259adc40',1,'EcranJeu']]],
+  ['labelj5_4',['labelJ5',['../d2/dc8/classEcranJeu.html#a1f91fa68ca73c5a16b1a64ea361b4391',1,'EcranJeu']]],
+  ['labelnom_5',['labelNom',['../de/d5c/classAffichageGUI.html#acb1f880f91c942db22f842535fda2182',1,'AffichageGUI::labelNom()'],['../d2/dc8/classEcranJeu.html#a606396df8d1c7b71f2fd07574e8d10c7',1,'EcranJeu::labelNom()']]],
+  ['labelpierre_6',['labelPierre',['../de/d5c/classAffichageGUI.html#ac8f48a7d4ec4c59750da61e0d3179f90',1,'AffichageGUI::labelPierre()'],['../d2/dc8/classEcranJeu.html#a7b639379d87904c90fccc3ce20782876',1,'EcranJeu::labelPierre()']]],
+  ['labelregles_7',['labelRegles',['../d2/dc8/classEcranJeu.html#a71b3d89e44c127b20c7a8c2469c22edd',1,'EcranJeu']]],
+  ['labelreglescore_8',['labelRegleScore',['../d2/dc8/classEcranJeu.html#a7ea4b88187ff46a543c465d30e71c394',1,'EcranJeu']]],
+  ['labelschantier_9',['labelsChantier',['../d2/dc8/classEcranJeu.html#aa0c711cd1135541316ac640719a4c4c1',1,'EcranJeu']]],
+  ['labelscore_10',['labelScore',['../de/d5c/classAffichageGUI.html#aa183aef7c604f92b7e8d1d1b032eb330',1,'AffichageGUI::labelScore()'],['../d2/dc8/classEcranJeu.html#a2f2d8bffd93c1ac807623a31f632d4b2',1,'EcranJeu::labelScore()']]],
+  ['largeurcentre_11',['largeurCentre',['../d3/d75/namespaceconstAffichageConsoleHex.html#ad1c2459cb2060d8fb56216a8f78739ac',1,'constAffichageConsoleHex']]],
+  ['largeurhex_12',['largeurHex',['../d3/d75/namespaceconstAffichageConsoleHex.html#a8d058f2a67cfa42c273b74eaea82fe72',1,'constAffichageConsoleHex']]],
+  ['layoutchampssaisies_13',['layoutChampsSaisies',['../d9/ddf/classEcranSaisieNoms.html#a10c8c05c2aad8aee768c9726cbcc6f29',1,'EcranSaisieNoms']]],
+  ['localpos_14',['localPos',['../d4/dfc/classHexagone.html#a3c72d948f768a1a9804c41fe27d24009',1,'Hexagone']]]
 ];
