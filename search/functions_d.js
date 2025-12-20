a4bc5b5fb93fa8f6f30f329496083fb8a52fcdc3
iceknith <iceknith@users.noreply.github.com>
2025-12-20 23:36:27 +0000

    deploy: bc68c983155088c4d32116c7facce829ac8c851e

--- search/functions_d.js
+++ search/functions_d.js
@@ -3,9 +3,10 @@ var searchData=
   ['paint_0',['paint',['../da/d63/classHexagoneGUIObjet.html#aad17c93f55a1ec419703035ed80c6023',1,'HexagoneGUIObjet']]],
   ['peut_5fetre_5fplacee_1',['peut_etre_placee',['../d4/dfc/classHexagone.html#afc8d977bc9a643d9a845f76c44d93e04',1,'Hexagone']]],
   ['peut_5fplacer_2',['peut_placer',['../d7/db5/classPlateau.html#a71a8a113eb2c9d3f87ecc71b4e930229',1,'Plateau']]],
-  ['place_5ftuile_3',['place_tuile',['../dd/d07/classJoueur.html#a8d224331463ad8fbeb51b8b7f07c0485',1,'Joueur']]],
-  ['placer_4',['placer',['../d7/db5/classPlateau.html#a03efba908af5746e414cbb1411b2b34e',1,'Plateau']]],
-  ['placetuile_5',['placeTuile',['../d1/de1/classJeu.html#ac026b185a9eb0989e9a8049ab1bcf607',1,'Jeu::placeTuile()'],['../dc/dcd/classJeuConsole.html#a44f1565d544e4bf6d7e306ba7b4eb987',1,'JeuConsole::placeTuile()'],['../da/d36/classJeuGUI.html#ab035f3c2378fd06a94baab4b310c0436',1,'JeuGUI::placeTuile()']]],
-  ['plateau_6',['Plateau',['../d7/db5/classPlateau.html#a07026170529dd928238ee45de0a797d7',1,'Plateau']]],
-  ['prendre_5ftuile_7',['prendre_tuile',['../df/d51/classChantier.html#ab7603d2b41161abc95bff277df26a491',1,'Chantier']]]
+  ['place_3',['Place',['../d9/d58/classPlace.html#ab5ae86c31e3a4c91800fb396d38e7a2b',1,'Place::Place()'],['../d9/d58/classPlace.html#ae2eec851dfd3b0e737ca0a6a813a4221',1,'Place::Place(Tuile *tuile_parent, const Vector2 &amp;localPos, CouleursAkropolis couleur)']]],
+  ['place_5ftuile_4',['place_tuile',['../dd/d07/classJoueur.html#a8d224331463ad8fbeb51b8b7f07c0485',1,'Joueur']]],
+  ['placer_5',['placer',['../d7/db5/classPlateau.html#a03efba908af5746e414cbb1411b2b34e',1,'Plateau']]],
+  ['placetuile_6',['placeTuile',['../d1/de1/classJeu.html#ac026b185a9eb0989e9a8049ab1bcf607',1,'Jeu::placeTuile()'],['../dc/dcd/classJeuConsole.html#a44f1565d544e4bf6d7e306ba7b4eb987',1,'JeuConsole::placeTuile()'],['../da/d36/classJeuGUI.html#ab035f3c2378fd06a94baab4b310c0436',1,'JeuGUI::placeTuile()']]],
+  ['plateau_7',['Plateau',['../d7/db5/classPlateau.html#a07026170529dd928238ee45de0a797d7',1,'Plateau']]],
+  ['prendre_5ftuile_8',['prendre_tuile',['../df/d51/classChantier.html#ab7603d2b41161abc95bff277df26a491',1,'Chantier']]]
 ];
